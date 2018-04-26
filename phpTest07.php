9e9546e802198426cdf626f243ab9e579fd1d587
ChrisHsiao <chrishsiao1020@gmail.com>
2018-04-26 10:23:56 +0800

    20180426 V2

--- phpTest07.php
+++ phpTest07.php
@@ -1,6 +1,42 @@
-<form action="phpTest08.php">
-    <input type="text" name="x" />
-    +
-    <input type="text" name="y" />
+<?php
+    $result = $x = $y = '';               //string
+    $op = 1;
+    if (isset($_GET['x'])) {       //isset() 弱型別語言，判斷是否存在
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        $op = $_GET['op'];
+        if($_GET['op'] == 1)
+            $result = $x + $y;      //integer
+        else if($_GET['op'] == 2)
+            $result = $x - $y;      //integer
+        else if($_GET['op'] == 3)
+            $result = $x * $y;      //integer
+        else if($_GET['op'] == 4)
+            $result = $x / $y;      //integer
+
+//        echo "{$x} + {$y} = {$result}";
+}
+?>
+
+<form>
+    <input type="text" name="x" value="<?php echo $x; ?>" />
+    <select name="op">
+        <option value="1" <?php if ($op == 1) {echo 'selected';} ?>>+</option>
+        <option value="2" <?php if ($op == 2) {echo 'selected';} ?>>-</option>
+        <option value="3" <?php if ($op == 3) {echo 'selected';} ?>>x</option>
+        <option value="4" <?php if ($op == 4) {echo 'selected';} ?>>/</option>
+    </select>
+    <input type="text" name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="=" />
+    <?php
+        echo $result;
+    ?>
 </form>
+
+
+<!--<form action="phpTest08.php">-->
+<!--    <input type="text" name="x" />-->
+<!--    +-->
+<!--    <input type="text" name="y" />-->
+<!--    <input type="submit" value="=" />-->
+<!--</form>-->
