438ebde97064c8b78e55b3c02353edb73e582bed
ChrisHsiao <chrishsiao1020@gmail.com>
2018-05-01 11:10:55 +0800

    20180501 V1

--- phpTest36.php
+++ phpTest36.php
@@ -1,3 +1,18 @@
 <?php
-    $fp = opendir("test");
-    echo gettype($fp);
+    $fp = opendir("test1");                               // 相對路徑
+    $fp = opendir("/Applications/MAMP/htdocs/test1");     // / --> 根，絕對路徑
+    //echo gettype($fp);            //目錄若不存在會傳回布林值
+
+    while ($file = readdir($fp)){   //readdir讀不到時傳回0
+        echo $file . '<br>';
+    }
+    closedir($fp);
+
+//    $s1 = readdir($fp);           //. 本目錄
+//    echo $s1 . '<br>';
+//    $s1 = readdir($fp);           //.. 上一層
+//    echo $s1 . '<br>';
+//    $s1 = readdir($fp);
+//    echo $s1 . '<br>';
+//    $s1 = readdir($fp);
+//    echo $s1 . '<br>';
